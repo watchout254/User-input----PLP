27b676e60ce97c2b12ab84694c943f2201f0faed
watchout254 <mukenyadaniel299@gmail.com>
2024-02-22 20:44:56 +0300

    User input my guy

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("****Welcome to Mukenyas App****")
+name = input("What is your name my guy/girl?: ")
+age = int(input("What is your age?: "))
+location = input("What is your current location?: ")
+
+print(f"Hello there {name},, you are {age} years old and live in {location}.")
+print("Have a lovely day.")
