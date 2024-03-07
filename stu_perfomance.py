08b42b571ef6860bf99a3d8cb351392d7a67912f
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-03-07 17:06:15 +0300

    Grading system

---
+++ stu_perfomance.py
@@ -0,0 +1,19 @@
+print("Welcome to Mukenya schools")
+print("All day everyday e got you.")
+
+marks = int(input("Kindly enter your marks: "));
+
+if marks > 80:
+    print("You have a Distinction, Big up yourself");
+
+elif marks < 80 and marks > 60:
+    print("You have Credit. Nice work");
+    
+elif marks < 60 and marks > 40:
+    print("You have performed Fairly. (Fair)");
+    
+else:
+    print("You have failed. Sorry my guy.")
+    
+    
+    
